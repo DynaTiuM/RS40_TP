118a1f4596ccaec5ee9912f96601e4f5de2d3b97
Raphael PERRIN <raphael.perrin754@gmail.com>
2023-05-12 13:59:05 +0200

    CRT Version

--- TP1/RSA_B_A (1).py
+++ TP1/RSA_B_A (1).py
@@ -1,8 +1,7 @@
 import hashlib
-import struct
 
 
-def home_mod_expnoent(x, y, n):  # exponentiation modulaire
+def home_mod_exponent(x, y, n):  # exponentiation modulaire
     return pow(x, y, n)
 
 
@@ -16,27 +15,49 @@ def home_ext_euclide(a, b):  # algorithme d'euclide étendu pour la recherche de
     return u
 
 
+def theoreme_chinois(c, d, n, p, q):
+    if p < q:
+        q, p = p, q
+
+    phip = (p - 1)
+    phiq = (q - 1)
+    dp = d % phip
+    dq = d % phiq
+
+    qinv = home_ext_euclide(q, p)
+
+    mp = home_mod_exponent(c, dp, p)
+    mq = home_mod_exponent(c, dq, q)
+
+    h = ((mp - mq) * qinv) % p
+    m = (mq + h * q) % n
+
+    return h, m
+
+
 def home_pgcd(a, b):  # recherche du pgcd
     if b == 0:
         return a
     else:
         return home_pgcd(b, a % b)
 
-def home_string_to_int(x): # pour transformer un string en int
-    z=0
+
+def home_string_to_int(x):  # pour transformer un string en int
+    z = 0
     for i in reversed(range(len(x))):
-        z=int(ord(x[i]))*pow(2,(8*i))+z
-    return(z)
+        z = int(ord(x[i])) * pow(2, (8 * i)) + z
+    return (z)
 
 
-def home_int_to_string(x): # pour transformer un int en string
-    txt=''
-    res1=x
-    while res1>0:
-        res=res1%(pow(2,8))
-        res1=(res1-res)//(pow(2,8))
-        txt=txt+chr(res)
+def home_int_to_string(x):  # pour transformer un int en string
+    txt = ''
+    res1 = x
+    while res1 > 0:
+        res = res1 % (pow(2, 8))
+        res1 = (res1 - res) // (pow(2, 8))
+        txt = txt + chr(res)
     return txt
+
 
 def mot20char():  # entrer le secret
     secret = input("donner un secret de 20 caractères au maximum : ")
@@ -52,8 +73,8 @@ def mot20char():  # entrer le secret
 # x2a = 3503815992030544427564583819137  # q
 
 # SHA256 :
-x1a = 308249334062031477258773846229713851551399395547349750630417 # p
-x2a = 846025411846152701157476644291808391711883118054770827939223 # q
+x1a = 308249334062031477258773846229713851551399395547349750630417  # p
+x2a = 846025411846152701157476644291808391711883118054770827939223  # q
 
 na = x1a * x2a  # n
 phia = ((x1a - 1) * (x2a - 1)) // home_pgcd(x1a - 1, x2a - 1)
@@ -67,8 +88,8 @@ da = home_ext_euclide(ea, phia) % phia  # exposant privé
 # x2b = 8842546075387759637728590482297  # q
 
 # SHA256 :
-x1b = 428298297181381295673584418828175452446566908925780625842667 # p
-x2b = 194718763443470226939248740594808481835603249509314749864867 # q
+x1b = 428298297181381295673584418828175452446566908925780625842667  # p
+x2b = 194718763443470226939248740594808481835603249509314749864867  # q
 
 nb = x1b * x2b  # n
 phib = ((x1b - 1) * (x2b - 1)) // home_pgcd(x1b - 1, x2b - 1)
@@ -95,16 +116,16 @@ print("voici la version en nombre décimal de ", secret, " : ")
 num_sec = home_string_to_int(secret)
 print(num_sec)
 print("voici le message chiffré avec la publique d'Alice : ")
-chif = home_mod_expnoent(num_sec, ea, na)
+chif = home_mod_exponent(num_sec, ea, na)
 print(chif)
 print("*******************************************************************")
-print("On utilise la fonction de hashage MD5 pour obtenir le hash du message", secret)
+print("On utilise la fonction de hashage SHA256 pour obtenir le hash du message", secret)
 Bhachis0 = hashlib.sha256(str(num_sec).encode('utf-8')).digest()  # SHA256 du message
 print("voici le hash en nombre décimal ")
 Bhachis3 = int.from_bytes(Bhachis0, byteorder='big')
 print(Bhachis3)
 print("voici la signature avec la clé privée de Bob du hachis")
-signe = home_mod_expnoent(Bhachis3, db, nb)
+signe = home_mod_exponent(Bhachis3, db, nb)
 print(signe)
 print("*******************************************************************")
 print("Bob envoie \n \t 1-le message chiffré avec la clé public d'Alice \n", chif, "\n \t 2-et le hash signé \n", signe)
@@ -112,18 +133,44 @@ print("*******************************************************************")
 x = input("appuyer sur entrer")
 print("*******************************************************************")
 print("Alice déchiffre le message chiffré \n", chif, "\nce qui donne ")
-dechif = home_int_to_string(home_mod_expnoent(chif, da, na))
+dechif = home_int_to_string(home_mod_exponent(chif, da, na))
 print(dechif)
+
 print("*******************************************************************")
 print("Alice déchiffre la signature de Bob \n", signe, "\n ce qui donne  en décimal")
-designe = home_mod_expnoent(signe, eb, nb)
+designe = home_mod_exponent(signe, eb, nb)
 print(designe)
+
 print("Alice vérifie si elle obtient la même chose avec le hash de ", dechif)
 Ahachis0 = hashlib.sha256(str(home_string_to_int(dechif)).encode('utf-8')).digest()  # SHA256 du message déchiffré
 Ahachis3 = int.from_bytes(Ahachis0, byteorder='big')
 print(Ahachis3)
 print("La différence =", Ahachis3 - designe)
 if Ahachis3 - designe == 0:
+    print("Alice : Bob m'a envoyé : ", dechif)
+else:
+    print("oups")
+
+# THEOREME CHINOIS
+print("*******************************************************************")
+print("Alice déchiffre le message chiffré grâce au théorème chinois :")
+dechifCRT = home_int_to_string(theoreme_chinois(chif, da, na, x1a, x2a)[1])
+print(dechifCRT)
+print("*******************************************************************")
+
+print("*******************************************************************")
+print("Alice déchiffre la signature de Bob grâce au théorème chinois :")
+designeCRT = home_mod_exponent(signe, eb, nb)
+print(designeCRT)
+print("*******************************************************************")
+
+print("Alice vérifie si elle obtient la même chose avec le hash de ", dechifCRT)
+
+Ahachis0 = hashlib.sha256(str(home_string_to_int(dechif)).encode('utf-8')).digest()  # SHA256 du message déchiffré
+Ahachis3 = int.from_bytes(Ahachis0, byteorder='big')
+print(Ahachis3)
+print("La différence =", Ahachis3 - designe)
+if Ahachis3 - designeCRT == 0:
     print("Alice : Bob m'a envoyé : ", dechif)
 else:
     print("oups")
